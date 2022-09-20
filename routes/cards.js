9cf34b5a9b5719d11bc6e8b1348af007a7a85615
Asik <asik.mirikov@mail.ru>
2022-09-20 20:42:49 +0300

    feat/Work-14

--- routes/cards.js
+++ routes/cards.js
@@ -1,16 +1,39 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
 const {
   createCard, deleteCard, getCards, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/', getCards);
 
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi
+      .string()
+      .required()
+      // eslint-disable-next-line no-useless-escape
+      .regex(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/),
+  }),
+}), createCard);
 
-router.delete('/:cardId', deleteCard);
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), deleteCard);
 
-router.put('/:cardId/likes', likeCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), likeCard);
 
-router.delete('/:cardId/likes', dislikeCard);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), dislikeCard);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,34 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
-  createUser, getUserById, getUsers, updateUser, updateAvatar,
+  getUserById, getUsers, updateUser, updateAvatar, getUserInfo,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 
-router.get('/:userId', getUserById);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().length(24),
+  }),
+}), getUserById);
 
-router.post('/', createUser);
+router.get('/me', getUserInfo);
 
-router.patch('/me', updateUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUser);
 
-router.patch('/me/avatar', updateAvatar);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi
+      .string()
+      // eslint-disable-next-line no-useless-escape
+      .regex(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)
+      .default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
+  }),
+}), updateAvatar);
 
 module.exports = router;
