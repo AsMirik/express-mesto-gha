57c87b66fd844d4cec7da00c200cde8ed639a039
Asik <asik.mirikov@mail.ru>
2022-09-21 02:56:52 +0300

    feat/Work-14.2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { celebrate, Joi, errors } = require('celebrate');
+// const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 
@@ -15,26 +15,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
+app.post('/signin', login);
 
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().default('Жак-Ив Кусто').min(2).max(30),
-    about: Joi.string().default('Исследователь').min(2).max(30),
-    avatar: Joi
-      .string()
-      // eslint-disable-next-line no-useless-escape
-      .regex(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)
-      .default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), createUser);
+app.post('/signup', createUser);
 
 app.use(auth);
 
@@ -45,7 +28,7 @@ app.use((req, res) => {
   res.status(404).send({ message: 'Страница не найдена' });
 });
 
-app.use(errors());
+// app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,17 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+// const { celebrate, Joi } = require('celebrate');
 const {
   getUserById, getUsers, updateUser, updateAvatar, getUserInfo,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 
-router.get('/:userId', celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24),
-  }),
-}), getUserById);
+router.get('/:userId', getUserById);
 
 router.get('/users/me', getUserInfo);
 
-router.patch('/me', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-  }),
-}), updateUser);
+router.patch('/me', updateUser);
 
-router.patch('/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi
-      .string()
-      // eslint-disable-next-line no-useless-escape
-      .regex(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)
-      .default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
-  }),
-}), updateAvatar);
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
